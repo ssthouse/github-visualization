094577b16a49c625c13483f9b3412e469401a426
ssthouse <ssthouse@163.com>
2018-05-15 11:56:07 +0800

    Add toolbar to show my github and github viz link

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,25 @@
 <template>
   <div id="app">
     <v-content>
+      <v-toolbar style="margin-bottom: 32px;">
+        <v-toolbar-side-icon></v-toolbar-side-icon>
+        <v-toolbar-title>Github Visualization</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-sm-and-down">
+          <v-btn flat href="https://github.com/ssthouse">
+            <v-avatar size=42>
+              <img src="https://avatars3.githubusercontent.com/u/10973821?s=460&v=4">
+            </v-avatar>
+            <span style="margin-left:8px;">About me</span>
+          </v-btn>
+          <v-btn flat href="https://github.com/ssthouse/github-visualization">
+            <v-avatar size=42>
+              <img src="https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png">
+            </v-avatar>
+            Source Code</v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+
       <v-container fluid>
         <router-view></router-view>
       </v-container>
@@ -45,6 +64,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+a {
+  color: inherit;
 }
 </style>
