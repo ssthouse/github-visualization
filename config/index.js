a5795ad1319ffe92c1520e4235694686015624a3
ssthouse <ssthouse@163.com>
2018-08-21 15:30:16 +0800

    Fix static assets path to relative path

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: './',
 
     /**
      * Source Maps
