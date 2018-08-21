ee94a34110069da62b45df5db726106b130f1dda
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-08-21 07:26:27 +0000

    Deploy ssthouse/github-visualization to github.com/ssthouse/github-visualization.git:gh-pages

--- config/index.js
+++ config/index.js
@@ -45,12 +45,12 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../docs/index.html'),
+    index: path.resolve(__dirname, '../index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../docs'),
+    assetsRoot: path.resolve(__dirname, '../'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: './',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
