8d8c29c3bd797d61dde3276b90d91bac661856a8
ssthouse <ssthouse@163.com>
2018-09-27 17:11:48 +0800

    Test add cude & change be to light gray

--- src/components/GithubViewThree.js
+++ src/components/GithubViewThree.js
@@ -7,30 +7,37 @@ class GithubViewThree {
 
   start() {
     var contaienrElement = document.getElementById(this.containerId)
-    var scene = new THREE.Scene()
-    var camera = new THREE.PerspectiveCamera(
+    this.scene = new THREE.Scene()
+    this.camera = new THREE.PerspectiveCamera(
       75,
       window.innerWidth / window.innerHeight,
       0.1,
       1000
     )
-    var renderer = new THREE.WebGLRenderer()
-    renderer.setSize(
+    this.camera.position.z = 5
+
+    this.renderer = new THREE.WebGLRenderer({ alpha: true })
+    this.renderer.setClearColor(0xeeeeee, 0.3)
+    this.renderer.setSize(
       contaienrElement.getBoundingClientRect().width,
       contaienrElement.getBoundingClientRect().height
     )
-    contaienrElement.appendChild(renderer.domElement)
+    contaienrElement.appendChild(this.renderer.domElement)
+    this.testAddCube()
 
+    this.animate()
+  }
+
+  testAddCube() {
     var geometry = new THREE.BoxGeometry(1, 1, 1)
     var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
     var cube = new THREE.Mesh(geometry, material)
-    scene.add(cube)
-    camera.position.z = 5
-    function animate() {
-      requestAnimationFrame(animate)
-      renderer.render(scene, camera)
-    }
-    animate()
+    this.scene.add(cube)
+  }
+
+  animate() {
+    requestAnimationFrame(() => this.animate())
+    this.renderer.render(this.scene, this.camera)
   }
 }
 
