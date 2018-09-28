0648454308672c94a45f195c843e2c9246a496d6
ssthouse <ssthouse@163.com>
2018-09-28 12:32:51 +0800

    Move balls in 3d version in to center

--- src/components/GithubViewThree.js
+++ src/components/GithubViewThree.js
@@ -9,20 +9,18 @@ class GithubViewThree {
   initScene() {
     var contaienrElement = document.getElementById(this.containerId)
     this.scene = new THREE.Scene()
-    this.camera = new THREE.PerspectiveCamera(
-      45,
-      window.innerWidth / window.innerHeight,
-      0.1,
-      1000
-    )
-    this.camera.position.z = 200
 
-    this.renderer = new THREE.WebGLRenderer({ alpha: true })
+    this.renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true })
     this.renderer.setClearColor(0xeeeeee, 0.3)
-    this.containerSize = contaienrElement.getBoundingClientRect().width
-    this.renderer.setSize(this.containerSize, this.containerSize)
+
+    const width = contaienrElement.getBoundingClientRect().width
+    const height = contaienrElement.getBoundingClientRect().height
+    this.containerSize = Math.min(width, height)
+    this.renderer.setSize(width, height)
     contaienrElement.appendChild(this.renderer.domElement)
-    // this.testAddCube()
+
+    this.camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 1000)
+    this.camera.position.z = 200
 
     // add light
     var spotLight = new THREE.SpotLight(0xffffff)
@@ -41,7 +39,7 @@ class GithubViewThree {
 
   addBall(xIndex, yIndex, radius) {
     var geometry = new THREE.SphereGeometry(radius, 32, 32)
-    var material = new THREE.MeshLambertMaterial({ color: 0xff0000 })
+    var material = new THREE.MeshLambertMaterial({ color: 0x008080 })
     var sphere = new THREE.Mesh(geometry, material)
     this.scene.add(sphere)
     sphere.position.set(xIndex, yIndex, 0)
@@ -61,23 +59,42 @@ class GithubViewThree {
     this.volumeScale = D3.scalePow()
       .exponent(1 / 3)
       .domain(D3.extent(this.reporitoryList, d => d.count))
-      .range([2, 20])
+      .range([2, 16])
+    this.indexScale = D3.scaleLinear()
+      .domain(D3.extent([0, 500]))
+      .range([0, 100])
 
     // use d3 to calculate the position of each circle
     this.simulation = D3.forceSimulation(this.reporitoryList)
       .force('charge', D3.forceManyBody())
-      .force('collide', D3.forceCollide().radius(40))
-      .force('forceX', D3.forceX(this.containerSize / 2).strength(0.05))
-      .force('forceY', D3.forceY(this.containerSize / 2).strength(0.05))
+      .force(
+        'collide',
+        D3.forceCollide().radius(d => this.volumeScale(d.count) + 12)
+      )
+      .force('forceX', D3.forceX(0).strength(0.05))
+      .force('forceY', D3.forceY(0).strength(0.05))
+      .on('end', function() {
+        self.updateIndex()
+      })
+  }
 
+  updateIndex() {
+    const self = this
     let virtualElement = document.createElement('svg')
     const circles = D3.select(virtualElement)
       .selectAll('circle')
       .data(this.reporitoryList)
-    circles.enter().each(function() {
-      const datum = D3.select(this).datum()
-      self.addBall(datum.x, datum.y, self.volumeScale(datum.count))
-    })
+    circles
+      .enter()
+      .merge(circles)
+      .each(function() {
+        const datum = D3.select(this).datum()
+        self.addBall(
+          self.indexScale(datum.x),
+          self.indexScale(datum.y),
+          self.volumeScale(datum.count)
+        )
+      })
   }
 }
 
